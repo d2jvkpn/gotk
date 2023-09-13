b59f4214261a3668e1e61623ac4abbc9f3cb3ab5
d2jvkpn <d2jvkpn@noreplay.local>
2023-09-14 06:52:54 +0800

    ....

--- ginx/log_test.go
+++ ginx/log_test.go
@@ -11,8 +11,8 @@ import (
 func TestCtx(t *testing.T) {
 	ctx := new(gin.Context)
 
-	SetDataField(ctx, "a", 1)
-	SetDataField(ctx, "b", 2)
+	SetData(ctx, map[string]any{"a": 1})
+	SetData(ctx, map[string]any{"b": 2})
 
 	data, ok := ctx.Get(GIN_Data)
 	fmt.Printf("ok: %t, data: %+v\n", ok, data)
