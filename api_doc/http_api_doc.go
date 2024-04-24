be638b30729d5707e56de6bf5fb2b70640a039f3
d2jvkpn <d2jvkpn@noreply.local>
2024-04-24 17:23:58 +0800

    ...

---
+++ api_doc/http_api_doc.go
@@ -0,0 +1,35 @@
+package api_doc
+
+import (
+	// "fmt"
+	"time"
+)
+
+type Field struct {
+	Type     string `mapstructure:"type"` // int32, int64, float32, bool, []xx
+	Key      string `mapstructure:"key"`
+	Value    string `mapstructure:"value"` // can be deserialize to Type
+	Required bool   `mapstructure:"required"`
+	Note     string `mapstructure:"note"`
+}
+
+type HttpAPI struct {
+	Name            string        `mapstructure:"name"`
+	Note            string        `mapstructure:"note"`
+	Interval        time.Duration `mapstructure:"interval"`
+	Request         string        `mapstructure:"request"` // method@path
+	Headers         []Field       `mapstructure:"headers"`
+	Parameters      []Field       `mapstructure:"parameters"`
+	Body            []Field       `mapstructure:"body"`
+	ResponseHeaders []Field       `mapstructure:"response_headers"`
+	ResponseBody    string        `mapstructure:"response_body"`
+}
+
+type HttpAPIs struct {
+	Name     string `mapstructure:"name"`
+	Basepath string `mapstructure:"basepath"`
+	public   struct {
+		Headers []Field // without subfield Type
+	} `mapstructure:"public"`
+	List []HttpAPI `mapstructure:"list"`
+}
