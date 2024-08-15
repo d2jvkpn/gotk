aab90cee2b6c92636ff1904e97867704a8a2d3f7
d2jvkpn <d2jvkpn@noreplay.local>
2024-08-15 16:34:14 +0800

    ...

--- cloud-tracing/examples/main.go
+++ cloud-tracing/examples/main.go
@@ -17,8 +17,8 @@ import (
 
 	"go.opentelemetry.io/otel/attribute"
 	"go.opentelemetry.io/otel/exporters/prometheus"
-	api "go.opentelemetry.io/otel/metric"
-	"go.opentelemetry.io/otel/sdk/metric"
+	otelmetric "go.opentelemetry.io/otel/metric"
+	sdkmetric "go.opentelemetry.io/otel/sdk/metric"
 )
 
 const meterName = "go.opentelemetry.io/otel/example/prometheus"
@@ -29,12 +29,12 @@ func main() {
 		ctx       context.Context
 		rng       *rand.Rand
 		exporter  *prometheus.Exporter
-		provider  *metric.MeterProvider
-		meter     api.Meter
-		opt       api.MeasurementOption
-		counter   api.Float64Counter
-		gauge     api.Float64ObservableGauge
-		histogram api.Float64Histogram
+		provider  *sdkmetric.MeterProvider
+		meter     otelmetric.Meter
+		opt       otelmetric.MeasurementOption
+		counter   otelmetric.Float64Counter
+		gauge     otelmetric.Float64ObservableGauge
+		histogram otelmetric.Float64Histogram
 	)
 
 	ctx = context.Background()
@@ -46,29 +46,32 @@ func main() {
 	if exporter, err = prometheus.New(); err != nil {
 		log.Fatal(err)
 	}
-	provider = metric.NewMeterProvider(metric.WithReader(exporter))
+	provider = sdkmetric.NewMeterProvider(sdkmetric.WithReader(exporter))
 	meter = provider.Meter(meterName)
 
 	// Start the prometheus HTTP server and pass the exporter Collector to it
 	go serveMetrics()
 
-	opt = api.WithAttributes(
+	opt = otelmetric.WithAttributes(
 		attribute.Key("A").String("B"),
 		attribute.Key("C").String("D"),
 	)
 
 	// This is the equivalent of prometheus.NewCounterVec
-	counter, err = meter.Float64Counter("foo", api.WithDescription("a simple counter"))
+	counter, err = meter.Float64Counter("foo", otelmetric.WithDescription("a simple counter"))
 	if err != nil {
 		log.Fatal(err)
 	}
 	counter.Add(ctx, 5, opt)
 
-	gauge, err = meter.Float64ObservableGauge("bar", api.WithDescription("a fun little gauge"))
+	gauge, err = meter.Float64ObservableGauge(
+		"bar",
+		otelmetric.WithDescription("a fun little gauge"),
+	)
 	if err != nil {
 		log.Fatal(err)
 	}
-	_, err = meter.RegisterCallback(func(_ context.Context, o api.Observer) error {
+	_, err = meter.RegisterCallback(func(_ context.Context, o otelmetric.Observer) error {
 		n := -10. + rng.Float64()*(90.) // [-10, 100)
 		o.ObserveFloat64(gauge, n, opt)
 		return nil
@@ -80,8 +83,8 @@ func main() {
 	// This is the equivalent of prometheus.NewHistogramVec
 	histogram, err = meter.Float64Histogram(
 		"baz",
-		api.WithDescription("a histogram with custom buckets and rename"),
-		api.WithExplicitBucketBoundaries(64, 128, 256, 512, 1024, 2048, 4096),
+		otelmetric.WithDescription("a histogram with custom buckets and rename"),
+		otelmetric.WithExplicitBucketBoundaries(64, 128, 256, 512, 1024, 2048, 4096),
 	)
 	if err != nil {
 		log.Fatal(err)
--- cloud-tracing/go.mod
+++ cloud-tracing/go.mod
@@ -3,11 +3,15 @@ module github.com/d2jvkpn/gotk/cloud-tracing
 go 1.23
 
 require (
+	github.com/prometheus/client_golang v1.19.1
 	go.opentelemetry.io/otel v1.28.0
 	go.opentelemetry.io/otel/exporters/otlp/otlptrace v1.28.0
 	go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc v1.28.0
+	go.opentelemetry.io/otel/exporters/prometheus v0.50.0
 	go.opentelemetry.io/otel/exporters/stdout/stdouttrace v1.28.0
+	go.opentelemetry.io/otel/metric v1.28.0
 	go.opentelemetry.io/otel/sdk v1.28.0
+	go.opentelemetry.io/otel/sdk/metric v1.28.0
 	go.opentelemetry.io/otel/trace v1.28.0
 	google.golang.org/grpc v1.65.0
 )
@@ -21,13 +25,9 @@ require (
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/grpc-ecosystem/grpc-gateway/v2 v2.21.0 // indirect
 	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
-	github.com/prometheus/client_golang v1.19.1 // indirect
 	github.com/prometheus/client_model v0.6.1 // indirect
 	github.com/prometheus/common v0.55.0 // indirect
 	github.com/prometheus/procfs v0.15.1 // indirect
-	go.opentelemetry.io/otel/exporters/prometheus v0.50.0 // indirect
-	go.opentelemetry.io/otel/metric v1.28.0 // indirect
-	go.opentelemetry.io/otel/sdk/metric v1.28.0 // indirect
 	go.opentelemetry.io/proto/otlp v1.3.1 // indirect
 	golang.org/x/net v0.28.0 // indirect
 	golang.org/x/sys v0.24.0 // indirect
