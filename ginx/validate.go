3397b4baded851faeff13764e4f29bc49ceeabc5
d2jvkpn <d2jvkpn@noreplay.local>
2024-08-31 08:56:59 +0800

    ...

--- ginx/validate.go
+++ ginx/validate.go
@@ -12,30 +12,30 @@ var (
 	_DefaultValidate = validator.New()
 )
 
+func validateToLower(field string) string {
+	var runes []rune
+
+	runes = []rune(field)
+	runes[0] = unicode.ToLower(runes[0])
+	return string(runes)
+}
+
 func Validate[T any](item *T) (err error) {
 	var (
 		errs   validator.ValidationErrors
 		fields []string
 	)
 
-	toLower := func(field string) string {
-		var runes []rune
-
-		runes = []rune(field)
-		runes[0] = unicode.ToLower(runes[0])
-		return string(runes)
-	}
-
 	if err = _DefaultValidate.Struct(item); err != nil {
-		errs = err.(validator.ValidationErrors)
+		errs, _ = err.(validator.ValidationErrors)
 		fields = make([]string, len(errs))
 		// fmt.Printf("==> error: %+v, filed: %q\n", err, field)
 
 		for i := range errs {
-			fields[i] = toLower(errs[i].Field())
+			fields[i] = validateToLower(errs[i].Field())
 		}
 
-		return errors.New(strings.Join(fields, ","))
+		return errors.New(strings.Join(fields, ", "))
 	}
 
 	return nil
