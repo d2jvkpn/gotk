e9cb8c4459064b81609f62c1ad07f68151e2ed9d
d2jvkpn <d2jvkpn@noreplay.local>
2024-10-06 17:49:14 +0800

    ...

---
+++ cloud/prom_metrics_api2.go
@@ -0,0 +1,53 @@
+package cloud
+
+import (
+	// "fmt"
+
+	"github.com/prometheus/client_golang/prometheus"
+)
+
+func PromMetricsAPI2(attrs []string) (fn func(string, float64, []string), err error) {
+	var (
+		codeCounter    *prometheus.CounterVec
+		requestLatency *prometheus.HistogramVec
+		// concurrentRequests prometheus.Gauge
+	)
+
+	// http_code, http response code summary
+	codeCounter = prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "http_code_total",
+			Help: "HTTP response codes counter",
+		},
+		append([]string{"api"}, attrs...),
+	)
+
+	requestLatency = prometheus.NewHistogramVec(
+		prometheus.HistogramOpts{
+			Name:    "http_latency",
+			Help:    "HTTP response latency milliseconds",
+			Buckets: []float64{10.0, 100.0, 200.0, 500.0, 1000.0, 5000.0}, // prometheus.DefBuckets,
+		},
+		[]string{"api"},
+	)
+	/*
+		concurrentRequests = prometheus.NewGauge(prometheus.GaugeOpts{
+			Name: "http_concurrent",
+			Help: "Number of concurrent requests being processed",
+		})
+	*/
+
+	for _, v := range []prometheus.Collector{codeCounter, requestLatency} {
+		if err = prometheus.Register(v); err != nil {
+			return nil, err
+		}
+	}
+
+	return func(api string, latency float64, codes []string) {
+		labelValues := append([]string{"api"}, codes...)
+
+		codeCounter.WithLabelValues(labelValues...).Inc()
+		requestLatency.WithLabelValues(api).Observe(latency)
+		// concurrentRequests.Dec()
+	}, nil
+}
